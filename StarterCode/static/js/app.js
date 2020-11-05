ca6ff4e1f4e624bce501cb8fde2e8e40d860ec0a
mcbien <mcbien1122@gmail.com>
2020-11-05 14:50:56 -0600

    Psuedocode complete

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -2,3 +2,75 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+
+
+// PSEUDO CODE
+// 1. user enters date in input date field
+// 2. listen for for button press of "Filter Table"; on button press 
+//    a. grab date entered from field
+//    b. filter data.js data to date entered
+//    c. add rows to table element in html via loop for all results via loop
+
+// print tableData
+// console.log(tableData);
+
+
+
+
+
+
+
+
+
+
+// Blank out table results
+
+
+// Select table body
+var tbody = d3.select("tbody");
+
+//Need loop
+for (var i = 0; j > tableData.length; j++) {
+    if (tableData[i]["datetime"] === dateEntered) {
+
+        // Append row to table
+        var row = tbody.append("tr");
+
+        // Append cell for Date
+        row.append("td").text(tableData["datetime"]);
+        // Append cell for City
+        row.append("td").text(tableData["city"]);
+        // Append cell for State
+        row.append("td").text(tableData["state"]);
+        // Append cell for Country
+        row.append("td").text(tableData["country"]);
+        // Append cell for Shape
+        row.append("td").text(tableData["shape"]);
+        // Append cell for Duration
+        row.append("td").text(tableData["durationMinutes"]);
+        // Append cell for Comments
+        row.append("td").text(tableData["comments"]);
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+// Select filter button
+
+// Listen for click
+
+// click
+
