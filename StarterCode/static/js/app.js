f7fb0b1485230ddd193a5c570063a4e9dfa9b463
mcbien <mcbien1122@gmail.com>
2020-11-05 16:56:57 -0600

    Working on event listener

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -9,84 +9,56 @@ var tableData = data;
 // 2. listen for for button press of "Filter Table"; on button press 
 //    a. grab date entered from field
 //    b. filter data.js data to date entered
-//    c. add rows to table element in html via loop for all results via loop
-
-// print tableData
-// console.log(tableData[0]["datetime"]);
-
-var dateEntered = "1/1/2010"
-var tableDataFirst = tableData[0]["datetime"]
-console.log(dateEntered)
-console.log(tableDataFirst)
+//    c. blank existing table
+//    d. add rows to table element in html via loop for all results via loop
 
 
+// Event listener - listen for button click
+var button = d3.select("#filter-btn")
+console.log(button)
+
+
+
+// Grab date entered field
+// Select input element
+// inputElement = d3.select("#datetime")
+// inputValue = inputElement.property("value");
+// console.log(inputValue);
+
+
+
+var dateEntered = "1/1/2010";
+
+// Select table body
+var tbody = d3.select("tbody");
 
 for (j = 0; j < tableData.length; j++) {
     if (dateEntered === tableData[j]["datetime"]) {
-        console.log("it worked")
+        console.log(tableData[j]["datetime"]);
 
-
+        // Append row to table
+        var row = tbody.append("tr");
+        // Append cell for Date
+        row.append("td").text(tableData[j]["datetime"]);
+        // Append cell for City
+        row.append("td").text(tableData[j]["city"]);
+        // Append cell for State
+        row.append("td").text(tableData[j]["state"]);
+        // Append cell for Country
+        row.append("td").text(tableData[j]["country"]);
+        // Append cell for Shape
+        row.append("td").text(tableData[j]["shape"]);
+        // Append cell for Duration
+        row.append("td").text(tableData[j]["durationMinutes"]);
+        // Append cell for Comments
+        row.append("td").text(tableData[j]["comments"]);
     }
 }
 
 
 
-// console.log(tableData.length)
-// console.log(dateEntered);
 
 
-
-
-
-
-
-for (var j = 0; j < tableData.length; j++) {
-    // if (tableData[j]["datetime"] == dateEntered) {
-    console.log(j);
-    // console.log(dateEntered);
-    // console.log(tableData[j]["datetime"]);
-
-    // }
-
-
-}
-
-
-
-
-
-
-
-
-
-
-// // Blank out table results
-
-
-// // Select table body
-// var tbody = d3.select("tbody");
-
-// //Need loop
-// for (var i = 0; j > tableData.length; j++) {
-//     if (tableData[i]["datetime"] === dateEntered) {
-
-        // // Append row to table
-        // var row = tbody.append("tr");
-
-        // // Append cell for Date
-        // row.append("td").text(tableData["datetime"]);
-        // // Append cell for City
-        // row.append("td").text(tableData["city"]);
-        // // Append cell for State
-        // row.append("td").text(tableData["state"]);
-        // // Append cell for Country
-        // row.append("td").text(tableData["country"]);
-        // // Append cell for Shape
-        // row.append("td").text(tableData["shape"]);
-        // // Append cell for Duration
-        // row.append("td").text(tableData["durationMinutes"]);
-        // // Append cell for Comments
-        // row.append("td").text(tableData["comments"]);
 
 
 
@@ -94,20 +66,3 @@ for (var j = 0; j < tableData.length; j++) {
 
 
     // }
-
-
-
-
-
-
-
-
-
-
-
-// Select filter button
-
-// Listen for click
-
-// click
-
